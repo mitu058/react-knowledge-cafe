0734c8826ad1c937c37935bfbef7f59e9f302519
mitu058 <mitum9796@gmail.com>
2024-10-22 10:12:40 +0600

    setup project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+     
+      <h1 className='text-5xl text-rose-300'>Knowledge Cafe</h1>
+    
+    </>
+  )
+}
+
+export default App
